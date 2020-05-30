128ada01be2e4b8084b8a7e0e089c5b8b40d377d
Julian <64177300+xeiks@users.noreply.github.com>
2020-05-30 20:55:47 -0300

    Add files via upload

--- core.js
+++ core.js
@@ -23,10 +23,8 @@ var esp = document.querySelector('.esp')
 		var siteUrl = getUrl .protocol + "//" + getUrl.host + "/";
 		const actualUrl = window.location.href;
 		var urlEsp = siteUrl + "esp.html";
-		console.log("urlEsp", urlEsp);
 		var urlEng = siteUrl + "eng.html";
 		var urlIndex =  siteUrl;
-		console.log("actualUrl",window.location.href);
 		if (locale === 'en' && actualUrl === urlIndex) window.location.href = urlEng;
 		if (locale === 'es' && actualUrl === urlIndex) window.location.href = urlEsp;
 		if (locale === 'en' && urlEsp === actualUrl) window.location.href = urlEng; 
