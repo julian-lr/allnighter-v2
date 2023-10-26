d65eedb6a3e582b454674ee7998a39bd58f3bf5a
julian-lr <j.laurisc@gmail.com>
2023-10-26 20:34:15 -0300

    Fixed the 'locale' issue, originally, if the .esp eventlistener was first, then it would only set the locale to 'es', and if the .eng eventlistener was first, then it would only set the locale to 'en', now it works for both cases.

--- js/local-storage-redirect.js
+++ js/local-storage-redirect.js
@@ -1,38 +1,43 @@
 // INDEX
 
-// SET "LOCALE" "EN" FOR REDIRECCION A INGLES
 (function () {
-  'use strict';
+  "use strict";
 
-  const eng = document.querySelector('.en');
-  eng.addEventListener('click', () => {
-    localStorage.setItem('locale', 'en');
+  document.addEventListener("DOMContentLoaded", function () {
+    const esp = document.querySelector(".esp");
+    const eng = document.querySelector(".eng");
+  
+    // If .esp class is found, then "locale" will be changed to es when it is clicked
+    if (esp) {
+      esp.addEventListener("click", () => {
+        localStorage.setItem("locale", "es");
+      });
+    // Else if .eng class is found, then "locale" will be changed to es when it is clicked
+    } else if (eng) {
+      eng.addEventListener("click", () => {
+        localStorage.setItem("locale", "en");
+      });
+    }
   });
 
-  // SET "LOCALE" "ES" FOR REDIRECCION A ESPAÑOL
-  const esp = document.querySelector('.es');
-  esp.addEventListener('click', () => {
-    localStorage.setItem('locale', 'es');
-  });
-
-  // RECIBE QUE "LOCALE" TIENE Y REDIRECCIONA
-  const locale = localStorage.getItem('locale');
+  // RECEIVES "LOCALE" AND REDIRECTS
+  const locale = localStorage.getItem("locale");
   const getUrl = window.location;
-  const siteUrl = getUrl.protocol + '//' + getUrl.host + '/';
+  const siteUrl = getUrl.protocol + "//" + getUrl.host + "/";
   const actualUrl = window.location.href;
-  const urlEsp = siteUrl + 'pages/esp.html';
-  const urlEng = siteUrl + 'pages/eng.html';
+  const urlEsp = siteUrl + "pages/esp.html";
+  const urlEng = siteUrl + "pages/eng.html";
 
-  if (locale === 'en' && actualUrl === siteUrl) {
+  if (locale === "en" && actualUrl === siteUrl) {
     window.location.href = urlEng;
   }
-  if (locale === 'es' && actualUrl === siteUrl) {
+  if (locale === "es" && actualUrl === siteUrl) {
     window.location.href = urlEsp;
   }
-  if (locale === 'en' && urlEsp === actualUrl) {
+  if (locale === "en" && urlEsp === actualUrl) {
     window.location.href = urlEng;
   }
-  if (locale === 'es' && urlEng === actualUrl) {
+  if (locale === "es" && urlEng === actualUrl) {
     window.location.href = urlEsp;
   }
 })();
